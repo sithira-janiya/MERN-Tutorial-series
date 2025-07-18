3de8dee4395ed636985905e0c4a63a860926c42a
Sithira Janiya <sithirajaniya87@gmail.com>
2025-07-18 10:48:43 +0530

    Added login page, header, footer, and cleaned test files

---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,44 @@
+// src/components/Header.js
+
+import React from "react";
+import {
+  Container,
+  Form,
+  FormControl,
+  Nav,
+  Navbar,
+  NavDropdown,
+} from "react-bootstrap";
+
+const Header = () => {
+  return (
+    <Navbar bg="primary" expand="lg" variant="dark">
+      <Container>
+        <Navbar.Brand href="/">Note Zipper</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <nav className="m-auto">
+            <Form className="d-flex">
+              <FormControl
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+            </Form>
+          </nav>
+          <Nav >
+            <Nav.Link href="#home">My Notes</Nav.Link>
+            <NavDropdown title="Sithira Janiya" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">My Profile</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">LogOut</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
